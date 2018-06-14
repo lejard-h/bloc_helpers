596b627359c263c95f6afeca4fa9706cf332f79d
Lejard Hadrien <hadrien.lejard@gmail.com>
2018-06-14 23:16:43 +0200

    doc

---
+++ README.md
@@ -0,0 +1,21 @@
+# Dart Bloc Pattern helper
+
+## What is Bloc pattern ?
+
+- [Flutter / AngularDart – Code sharing, better together (DartConf 2018)](https://youtu.be/PLHln7wHgPE)
+- [Build reactive mobile apps in Flutter](https://youtu.be/RS36gBEp8OI)
+- [Build reactive mobile apps in Flutter - companion article](https://medium.com/flutter-io/build-reactive-mobile-apps-in-flutter-companion-article-13950959e381)
+
+## bloc_helpers
+
+This package contain helper class and common Bloc Pattern
+
+- Bloc (base to implement Bloc pattern)
+- RequestBloc
+- CachedRequestBloc
+- SelectorBloc
+
+## Contributions
+
+This repository is open to Pull Request and new idea :)
+I hope we can get a long list of generic and reusable Bloc.
--- lib/src/selector.dart
+++ lib/src/selector.dart
@@ -3,17 +3,48 @@ import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 import 'bloc.dart';
 
+/// Bloc implementing a selection behavior
+///
+/// Unique value selector
+/// ```dart
+/// final selector = new SelectorBloc<String>();
+///
+/// selector.selected.first; // []
+///
+/// selector.selectSink.add('foo'); // ['foo']
+/// selector.selectSink.add('foo'); // ['foo']
+/// selector.selectAllSink.add(['foo', 'toto']); // ['foo', 'toto']
+/// selector.selectSink.add('bar); // ['foo', 'toto', 'bar']
+///
+/// selector.unselect.add('foo'); // ['toto', 'bar']
+/// selector.unselectAll.add(null); // []
+///
+/// or
+///
+/// ```dart
+/// final selector = new SelectorBloc<String>(unique: false);
+///
+/// selector.selectSink.add('foo');
+/// selector.selectSink.add('foo');
+/// selector.selectSink.add('bar);
+///
+/// selector.selected.first; /// [ 'foo', 'foo', 'bar' ]
+/// ```
 class SelectorBloc<T> extends Bloc {
   final bool unique;
 
-  final _selectedBehavior = new BehaviorSubject<List<T>>();
+  final BehaviorSubject<Iterable<T>> _selectedBehavior;
 
   final _selectPublisher = new PublishSubject<T>();
-  final _selectAllPublisher = new PublishSubject<List<T>>();
+  final _selectAllPublisher = new PublishSubject<Iterable<T>>();
   final _unselectPublisher = new PublishSubject<T>();
   final _unselectAllPublisher = new PublishSubject<void>();
 
-  SelectorBloc({this.unique: true}) {
+  SelectorBloc({this.unique: true, Iterable<T> seedValue: const <T>[]})
+      : _selectedBehavior = unique
+            ? new BehaviorSubject<Set<T>>(seedValue: new Set<T>.from(seedValue))
+            : new BehaviorSubject<List<T>>(
+                seedValue: new List<T>.from(seedValue)) {
     onSelect.listen((v) => _add([v]));
     onSelectAll.listen(_add);
 
@@ -22,12 +53,8 @@ class SelectorBloc<T> extends Bloc {
   }
 
   void _add(List<T> values) {
-    final toAdd = unique
-        ? values.where((v) => !_selectedBehavior.value.contains(v))
-        : values;
-
     _selectedBehavior.add(
-      _selectedBehavior.value.toList()..addAll(toAdd),
+      _selectedBehavior.value.toList()..addAll(values),
     );
   }
 
@@ -47,23 +74,34 @@ class SelectorBloc<T> extends Bloc {
     super.dispose();
   }
 
-  Stream<List<T>> get selected => _selectedBehavior;
+  /// Stream of selected item
+  /// Emit the last value when listen
+  Stream<Iterable<T>> get selected => _selectedBehavior;
 
+  /// Emit event when [selectSink.add]
   Stream<T> get onSelect => _selectPublisher.stream;
 
+  /// Emit event when [unselectSink.add]
   Stream<T> get onUnselect => _unselectPublisher.stream;
 
-  Stream<List<T>> get onSelectAll => _selectAllPublisher.stream;
+  /// Emit event when [selectAllSink.add]
+  Stream<Iterable<T>> get onSelectAll => _selectAllPublisher.stream;
 
+  /// Emit event when [unselectAllSink.add]t
   Stream<void> get onUnselectAll => _unselectAllPublisher.stream;
 
+  /// Sink to unselect an item
   Sink<T> get unselectSink => _unselectPublisher.sink;
 
+  /// Sink to select an item
   Sink<T> get selectSink => _selectPublisher.sink;
 
-  Sink<List<T>> get selectAllSink => _selectAllPublisher.sink;
+  /// Sink to select multiple item
+  Sink<Iterable<T>> get selectAllSink => _selectAllPublisher.sink;
 
+  /// Sink to unselect every item in the list
   Sink<void> get unselectAllSink => _unselectAllPublisher.sink;
 
-  Sink<List<T>> get loadSink => _selectedBehavior.sink;
+  /// Sink to manually update the selected list
+  Sink<Iterable<T>> get loadSink => _selectedBehavior.sink;
 }
